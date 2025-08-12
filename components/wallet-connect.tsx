275f9d662db17d15022ea5d1b5b520f62ed15cca
gascheckking <warpaiactivity1@gmail.com>
2025-08-12 12:14:33 +0200

    Update wallet-connect.tsx

--- components/wallet-connect.tsx
+++ components/wallet-connect.tsx
@@ -1,27 +1,26 @@
-'use client'
+"use client";
 
-import { WagmiProvider, createConfig } from 'wagmi'
-import { base } from 'viem/chains'
-import { ConnectKitProvider, getDefaultConfig } from 'connectkit'
-import { ConnectKitButton } from 'connectkit'
-
-const config = createConfig(
-  getDefaultConfig({
-    chains: [base],
-    walletConnectProjectId: '', // Add your ID
-  })
-)
-
-function WalletConnectInner() {
-  return <ConnectKitButton />
-}
+import { ConnectKitButton } from "connectkit";
+import { useAccount, useDisconnect } from "wagmi";
 
 export function WalletConnect() {
+  const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+
   return (
-    <WagmiProvider config={config}>
-      <ConnectKitProvider>
-        <WalletConnectInner />
-      </ConnectKitProvider>
-    </WagmiProvider>
-  )
+    <div>
+      <ConnectKitButton />
+      {isConnected && (
+        <div>
+          <p>Connected: {address}</p>
+          <button
+            onClick={() => disconnect()}
+            className="bg-red-500 text-white px-4 py-2 rounded"
+          >
+            Disconnect
+          </button>
+        </div>
+      )}
+    </div>
+  );
 }
