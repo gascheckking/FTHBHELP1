a6644d023a41fef80b7860ccdb0ae5587e9cccd7
gascheckking <warpaiactivity1@gmail.com>
2025-08-12 11:21:49 +0200

    Create community-feed.tsx

---
+++ components/community-feed.tsx
@@ -0,0 +1,9 @@
+export function CommunityFeed() {
+  return (
+    <div>
+      <h2>Community Feed</h2>
+      <p>Latest casts from /communitybase on Farcaster.</p>
+      {/* TODO: Integrate Farcaster API */}
+    </div>
+  )
+}
