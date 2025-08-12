d9740d304cc79a60d13398c737d48240c0c0504b
gascheckking <warpaiactivity1@gmail.com>
2025-08-12 11:16:51 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Chat } from '@/components/chat'
+
+export default function IndexPage() {
+  return (
+    <div className="flex flex-col h-screen overflow-hidden">
+      <Chat />
+    </div>
+  )
+}
