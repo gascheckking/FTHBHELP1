f477604b7d6c0c472b8b4e685869c8395c2317be
gascheckking <warpaiactivity1@gmail.com>
2025-08-12 16:57:08 +0200

    Create community-feed.tsx

---
+++ src/components/community-feed.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export const CommunityFeed = () => {
+  return (
+    <div className="mt-4">
+      <h3 className="text-lg font-semibold">Community Feed</h3>
+      <p>Latest community updates will appear here.</p>
+    </div>
+  );
+};
